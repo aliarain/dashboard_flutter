e696cb356132f1086524207e40e30bfe206f681e
Ali Arain <deistali@outlook.com>
2021-10-06 15:17:39 +0500

    Initial Commit 🔥

---
+++ README.md
@@ -0,0 +1,24 @@
+# dashboard_flutter
+A Dashboard develop with flutter web.
+
+**Pull Request Are Welcome**
+
+**Project Structure**
+*lib/*
+
+ - routing (
+ - widget
+ - helpers
+ - constants
+ - controller
+ - pages
+ - main.dart
+**List of Packages used In This Project**
+ - google_fonts
+ - getx
+ - chart_flutter
+ - data_table_2
+
+**Install Packages**
+
+> `flutter pub get`
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:dashboard_flutter/layout.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Dashboard ",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.mulishTextTheme(
+          Theme.of(context).textTheme
+        ).apply(
+          bodyColor: Colors.black
+        ),
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder()
+          }),
+          primaryColor: Colors.blue,
+      ),
+      home: SiteLayout(),
+    );
+  }
+}
